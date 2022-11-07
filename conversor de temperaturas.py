30645eabb0b69afcde160b83e584d876c3831f81
BiraBalaZ <85596186+BiraBalaZ@users.noreply.github.com>
2022-11-07 20:30:13 -0300

    Add files via upload

---
+++ conversor de temperaturas.py
@@ -0,0 +1,7 @@
+celsius = int(input('Digite aqui um valor em Cº\n>>>'))
+
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+
+print(f'{celsius}Cº equivale à {fahrenheit}Fº')
+print(f'{celsius}Cº equivale à {kelvin}Kº')
